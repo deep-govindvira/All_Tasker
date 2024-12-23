fc6cea3b63e4d380003d2396897c8d6741322cf2
Deep Govindvira <deepgovindvira@gmail.com>
2024-12-24 00:57:04 +0530

    include navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 import Home from './home/Home';
+import Todo from './todo/Todo';
+import Navbar from './Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <div>
+          <Navbar/>
+        </div>
         <Routes>
-          <Route path='/' element={<Home />} />          
+          <Route path='/' element={<Home />} />
+          <Route path='/Todo' element={<Todo />} />
         </Routes>
       </BrowserRouter>
+      <div className="fixed-bottom bg-dark text-light">Deep Govindvira © 2024</div>
     </div>
   );
 }
