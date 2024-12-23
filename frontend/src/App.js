c72821b8331578407af8e6c8829dd02bda428114
Deep Govindvira <deepgovindvira@gmail.com>
2024-12-23 23:29:08 +0530

    add home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Home from './home/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />          
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
