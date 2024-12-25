b8f792bb8f0d21d6b1402d76f3e5731afa85dd41
Deep Govindvira <deepgovindvira@gmail.com>
2024-12-25 23:47:00 +0530

    add draw functionlity

---
+++ frontend/src/draw/Draw.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Draw = () => {
+  return (
+    <div>
+      <iframe
+        src="https://witeboard.com/7710daa0-c2e4-11ef-ad6e-255d089a146b"
+        width="100%"
+        height="600"
+        frameBorder="0"
+        title="Witeboard"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Draw;
